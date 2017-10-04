012ffe1e900c4700d9add476e8da3d17001fc6bd
fire855 <thefire855@gmail.com>
2017-10-04 23:12:07 +0200

    Move everything to vendor
    Conflicts:
    	etc/firmware/WMT_SOC.cfg
    	etc/firmware/catcher_filter_1_lwg_n.bin
    	etc/firmware/dsp_1_lwg_n.bin
    	etc/firmware/modem_1_lwg_n.img
    	etc/firmware/mt6627/mt6627_fm_v1_coeff.bin
    	etc/firmware/mt6627/mt6627_fm_v1_patch.bin
    	etc/firmware/mt6627/mt6627_fm_v2_coeff.bin
    	metal-vendor-blobs.mk
    	note3_firmware/firmware/WMT_SOC.cfg
    	note3_firmware/firmware/catcher_filter_1_lwg_n.bin
    	note3_firmware/firmware/dsp_1_lwg_n.bin
    	note3_firmware/firmware/modem_1_lwg_n.img
    	note3_firmware/firmware/mt6627/mt6627_fm_v1_coeff.bin
    	note3_firmware/firmware/mt6627/mt6627_fm_v1_patch.bin
    	note3_firmware/firmware/mt6627/mt6627_fm_v2_coeff.bin
    	vendor/bin/etsd
    	vendor/bin/mxg2320d
    	vendor/etc/firmware/ROMv2_lm_patch_1_0_hdr.bin
    	vendor/etc/firmware/ROMv2_lm_patch_1_1_hdr.bin
    	vendor/etc/firmware/WIFI_RAM_CODE_6735
    	vendor/etc/firmware/WMT_SOC.cfg
    	vendor/etc/firmware/catcher_filter_1_lwg_n.bin
    	vendor/etc/firmware/dsp_1_lwg_n.bin
    	vendor/etc/firmware/modem_1_lwg_n.img
    	vendor/etc/firmware/mt6627/mt6627_fm_v1_coeff.bin
    	vendor/etc/firmware/mt6627/mt6627_fm_v1_patch.bin
    	vendor/etc/firmware/mt6627/mt6627_fm_v2_coeff.bin
    	vendor/lib/libmtkcam_fwkutils.so
    	vendor/lib/libmtkcam_metadata.so
    	vendor/lib/libmtkcam_stdutils.so
    	vendor/lib64/hw/fingerprint.mt6753.so
    	vendor/lib64/libEtsdc.so
    	vendor/lib64/libFpEts.so
    	vendor/lib64/libmtkcam_fwkutils.so
    	vendor/lib64/libmtkcam_metadata.so
    	vendor/lib64/libmtkcam_stdutils.so

--- note3lite-firmware-blobs.mk
+++ note3lite-firmware-blobs.mk
@@ -1,1 +1,1 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/note3/note3lite_firmware/firmware,system/etc/firmware)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/note3/note3lite_firmware/firmware,system/vendor/etc/firmware)
