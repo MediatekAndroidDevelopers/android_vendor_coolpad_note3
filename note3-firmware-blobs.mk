ba071fa37afd604a9225f5053c1b9b7989881e15
DerTeufel <dominik-kassel@gmx.de>
2017-12-03 14:39:49 +0100

    firmware files should be placed to vendor/firmware
    hexedit ccci blobs accordingly

--- note3-firmware-blobs.mk
+++ note3-firmware-blobs.mk
@@ -1,1 +1,1 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/note3/note3_firmware/firmware,system/vendor/etc/firmware)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/note3/note3_firmware/firmware,system/vendor/firmware)
