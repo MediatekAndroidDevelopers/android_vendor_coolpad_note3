56ccbb5e8fbbfa64dde727ddd1d040629b826ae6
DerTeufel <dominik-kassel@gmx.de>
2017-12-05 12:45:51 +0100

    fingerprint: updates for O
    hex edit blobs for new location (/vendor)
    add vendor init service
    remove fingerprintd

--- Android.mk
+++ Android.mk
@@ -4,11 +4,12 @@ ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),note3 note3lite))
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := lib_fpc_tac_shared
-LOCAL_SRC_FILES_64 := lib64/lib_fpc_tac_shared.so
-LOCAL_SRC_FILES_32 := lib/lib_fpc_tac_shared.so
+LOCAL_SRC_FILES_64 := vendor/lib64/lib_fpc_tac_shared.so
+LOCAL_SRC_FILES_32 := vendor/lib/lib_fpc_tac_shared.so
 LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
+LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
 endif
