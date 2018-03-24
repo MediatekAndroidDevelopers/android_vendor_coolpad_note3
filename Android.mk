ccc7a76a89703c3401c7a30694f2e0681aafc79c
DerTeufel <dominik-kassel@gmx.de>
2018-03-24 12:27:24 +0100

    use some cam blobs from wiko fever, as our stock ones don't work

--- Android.mk
+++ Android.mk
@@ -12,4 +12,13 @@ LOCAL_MODULE_SUFFIX := .so
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libdpframework
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_SRC_FILES_32 := vendor/lib/libdpframework.so
+LOCAL_SRC_FILES_64 := vendor/lib64/libdpframework.so
+LOCAL_MULTILIB := both
+include $(BUILD_PREBUILT)
+
 endif
